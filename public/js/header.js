b434554d54cc50c0fa9c0bc1155c0b0142bae9fe
이경찬 <02skdisk@naver.com>
2023-07-19 18:15:10 +0900

    전체css수정

--- public/js/header.js
+++ public/js/header.js
@@ -12,11 +12,27 @@ hamBtn.addEventListener("click",(e)=>{
     if(toggle){
         hamBtnCross.style.transform = "translate(-50%,-50%) rotate(180deg)"
         gnbBg.style.transform = "translateY(0%)"
+        hamColorB()
         toggle = !toggle
     }
     else if(toggle === false){
         toggle = !toggle;
         hamBtnCross.style.transform = "translate(-50%,-50%) rotate(90deg)"
         gnbBg.style.transform = "translateY(-200%)"
+        hamColorWh()
     }
 })
+// 햄버거메뉴 흰색 함수
+function hamColorWh(){
+    for(let i=0; i< hamBtnSpan.length; i++){
+            hamBtnSpan[i].style.backgroundColor = '#fff'
+        }
+        hamBtn.style.color = '#fff'
+};
+// 햄버거메뉴 검은색 함수
+function hamColorB(){
+    for(let i=0; i< hamBtnSpan.length; i++){
+            hamBtnSpan[i].style.backgroundColor = '#000'
+        }
+        hamBtn.style.color = '#000'
+};
